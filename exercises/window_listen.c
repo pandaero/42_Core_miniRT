2a833531287f8fd8e5fc09fe3d646a59b0f5e8a2
Piotr Biedermann <piotr.biedermann84@gmail.com>
2023-01-07 18:44:18 +0100

    put a pixel - amazing

--- exercises/window_listen.c
+++ exercises/window_listen.c
@@ -31,16 +31,45 @@ int	keys(int key, t_vars *mlxvars)
 	return (0);
 }
 
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	t_vars	*vars;
 	void	*image;
+	int		width;
+	int		height;
 
+	width = 300;
+	height = 500;
 	vars->mlx = mlx_init();
 	if (!vars->mlx)
 		return(0);
-	vars->window = mlx_new_window(vars->mlx, 500, 500, "Title");
-	image = mlx_new_image(vars->mlx, 500, 500);
+	vars->window = mlx_new_window(vars->mlx, width, height, "Title");
+	image = mlx_new_image(vars->mlx, width, height);
+	my_mlx_pixel_put(image, 5, 5, 0x00FF0000);
+	// while (width >= 0)
+	// {
+	// 	while(height >= 0)
+	// 	{
+	// 		height--;
+	// 	}
+	// 	width--;
+	// }
 	mlx_put_image_to_window(vars->mlx, vars->window, image, 0, 0);
 	mlx_hook(vars->window, 17, 0, closing, vars);
 	mlx_hook(vars->window, 3, 0, keys, vars);
