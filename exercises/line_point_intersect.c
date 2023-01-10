d7a4b4036c7aae0a105c382c1390ecdc40ce02aa
piotr biedermann <piotr.biedermann84@gmail.com>
2023-01-10 16:47:56 +0100

    changed file name, cleaned line point, added line shphere file

--- exercises/line_point_intersect.c
+++ exercises/line_point_intersect.c
@@ -1,24 +1,24 @@
-// Exercise to open a window and record events.
-// Using minilibx-opengl-20191021
-// From root: cc -framework OpenGL -framework AppKit exercises/window_listen.c -lmlx
-
-#include "../minilibx_opengl_20191021/mlx.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define a 2
+#define coordinate_x 1
+#define coordinate_y 2
+/*Checks if the given point x1 and y2 intersects with the function a*x = y
+Simple introduction to the concept of intersection. This implementation would be too resource heavy.*/
 int	main(void)
 {
 	int		x1;
 	int		y2;
 	int		x, y;
 
-	x1 = 1;
-	y2 = 2;
+	x1 = coordinate_x;
+	y2 = coordinate_y;
 	x = 0;
 	while (x < 10)
 	{
-		y = 2 * x;
-		if (x1 * 2 == y2)
+		if (x1 * a == y2)
 		{
 			printf("It intesects\n");
 			return(0);
