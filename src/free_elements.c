303903428e356c6e21de4271f6e0d0e79d7b6f5c
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-10 21:30:38 +0100

    added freeing function for rays and corrected name for vector freeings

--- src/free_elements.c
+++ src/free_elements.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 17:47:39 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/01/10 18:49:06 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/01/10 21:27:51 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,18 @@ void	free_direction(t_direction *direction)
 }
 
 //Function frees all the allocations belonging to a vector object.
-void	vector_free(t_vector *vector)
+void	free_vector(t_vector *vector)
 {
 	free_point(vector->start);
 	free_point(vector->end);
 	free_direction(vector->dir);
 	free(vector);
+}
+
+//Function frees all the allocations belonging to a ray object.
+void	free_ray(t_ray *ray)
+{
+	free_point(ray->ray_orig);
+	free_direction(ray->ray_dir);
+	free(ray);
 }
