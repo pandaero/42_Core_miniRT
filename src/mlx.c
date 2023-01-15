8d622cb74c86d52db3dbef93b3be7b0c310300b6
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-15 23:17:51 +0100

    added mlx quick pixel function

---
+++ src/mlx.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 22:33:54 by pandalaf          #+#    #+#             */
+/*   Updated: 2023/01/15 22:34:21 by pandalaf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minirt.h"
+
+//Function places a pixel in an image more quickly than with the pixel_put fn.
+void	quick_put_pixel(t_imgdata *data, int x, int y, int color)
+{
+	char	*dest;
+
+	dest = 0;
+	if (x >= 0 && x < WIN_WIDTH && y >= 0 && y < WIN_HEIGHT)
+	{
+		dest = data->address + (y * data->line_len + x * (data->bits_pp / 8));
+		*(unsigned int *)dest = color;
+	}
+}
