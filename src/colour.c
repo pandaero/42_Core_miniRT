0b4ee16fdf1b7d4b604a4d41dc18599e7baecba8
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-15 23:16:41 +0100

    added defined colours, and corrected struct member calling

--- src/colour.c
+++ src/colour.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 16:41:50 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/01/15 16:55:25 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/01/15 22:42:49 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ t_colour	colour_ambient(t_colour colour, t_ambient ambient)
 {
 	t_colour	resulting;
 
-	if (colour > (int) 0.5 * ambient->colour)
-		resulting = colour + (int) ambient->ratio * ambient->colour;
-	else if (colour < (int) 0.5 * ambient->colour)
-		resulting = colour - (int) ambient->ratio * ambient->colour;
+	if (colour > (int) 0.5 * ambient.colour)
+		resulting = colour + (int) ambient.ratio * ambient.colour;
+	else if (colour < (int) 0.5 * ambient.colour)
+		resulting = colour - (int) ambient.ratio * ambient.colour;
 	else
 		resulting = colour;
-	if (resulting > 0x00FFFFFF)
-		resulting = 0x00FFFFFF;
-	if (resulting < 0x00000000)
-		resulting = 0x00000000;
+	if (resulting > WHITE)
+		resulting = WHITE;
+	if (resulting < BLACK)
+		resulting = BLACK;
 	return (resulting);
 }
