ad4987543a59fd0c815924988b39ad92721866f9
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-16 15:22:38 +0100

    shortened some comments and clarified indentation

--- src/intersect_sphere.c
+++ src/intersect_sphere.c
@@ -6,22 +6,20 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 22:22:24 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/01/16 15:03:52 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/01/16 15:22:15 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minirt.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 /*
 Added a funcition that normalizes the direction courtesy of Pablo.
 use:
 cc ray_sphere.c operations.c input.c -lm
 */
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <math.h>
-
 typedef struct s_Vector3
 {
 	double	x;
@@ -44,9 +42,7 @@ t_Vector3 *direction, double distance)
 	return (point_of_intersection);
 }
 
-// Function that finds a point of 
-// intersection between a ray 
-// and a sphere of a defined radius and center in space.
+// Function determines whether a ray and a sphere intersect.
 // Nomenclature:
 // 	rp - Resulting from subtracting center of sphere from ray origin.
 // 	t0 - distance of normalized direction vector to a point of intersection
@@ -62,16 +58,16 @@ bool	ray_sphere_intersection(t_ray *ray, t_sphere *sphere)
 	vec_ray_dir = vector_scale_direction(1, ray->ray_dir);
 	rp = vector_two_points(ray->ray_orig, sphere->centre);
 	rs.y = pow(vector_dot(rp, vec_ray_dir), 2) - \
-	(vector_dot(rp, rp)) + pow(sphere->radius, 2);
+			(vector_dot(rp, rp)) + pow(sphere->radius, 2);
 	if (rs.y >= 0)
 	{
 		rs.intersect = true;
 		rs.t0 = (vector_dot(rp, vec_ray_dir)) - \
-		sqrt((pow(vector_dot(rp, vec_ray_dir), 2)) - \
-		(vector_dot(rp, rp)) + (pow(sphere->radius, 2)));
+				sqrt((pow(vector_dot(rp, vec_ray_dir), 2)) - \
+				(vector_dot(rp, rp)) + (pow(sphere->radius, 2)));
 		rs.t1 = (vector_dot(rp, vec_ray_dir)) + \
-		sqrt((pow(vector_dot(rp, vec_ray_dir), 2)) - \
-		(vector_dot(rp, rp)) + (pow(sphere->radius, 2)));
+				sqrt((pow(vector_dot(rp, vec_ray_dir), 2)) - \
+				(vector_dot(rp, rp)) + (pow(sphere->radius, 2)));
 	}
 	else
 		rs.intersect = false;
