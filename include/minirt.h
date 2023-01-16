a7b6f3a7b3231f2cb151220f5a49506430ec11e3
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-16 14:20:44 +0100

    corrected white colour transparency to make it opaque

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 16:16:35 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/01/15 23:51:00 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/01/16 14:20:14 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@
 # define VIEW_SCALING 1
 // Colours
 # define BLACK 0x00000000
-# define WHITE 0xFFFFFFFF
+# define WHITE 0x00FFFFFF
 
 // ================================= TYPE PROTOTYPES ===========================
 typedef enum element		t_element;
