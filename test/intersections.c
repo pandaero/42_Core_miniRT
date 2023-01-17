ae2aeb3b812cc9b1cf122edbea12757188e97232
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-18 00:00:37 +0100

    major revamp for intersections test

--- test/intersections.c
+++ test/intersections.c
@@ -2,7 +2,7 @@
 // Using minilibx-opengl-20191021
 // From root: cc -framework OpenGL -framework AppKit
 // Using minilibx-linux
-// From root: cc test/intersections.c src/calculation/* src/elements/* src/intersection/* src/memory/* src/program/* libft/libft.a minilibx-linux/libmlx_Linux.a -lX11 -lXext -lm
+// From root: cc test/test.c test/intersections.c src/calculation/* src/elements/* src/intersection/* src/memory/* src/program/* src/object/* libft/libft.a minilibx-linux/libmlx_Linux.a -lX11 -lXext -lm
 // From folder: cc intersection.c ../src/*.c ../src/intersections/*.c ../minilibx-linux/libmlx_Linux.a ../libft/libft.a -lX11 -lXext -lm
 
 #include "./test.h"
@@ -39,27 +39,14 @@ int	keys(int key, t_mlxdata *mlxvars)
 //Test program for a single sphere in view.
 int	main(void)
 {
-	t_screen	*screen;
-	t_camera	*cam;
-	t_point		*cam_loc;
-	t_point		*cam_point;
-	t_direction	*cam_view_dir;
-	t_ray		*ray;
-	t_point		*sphere_centre;
-	t_sphere	*sphere;
-	t_ambient	*ambient;
 	t_mlxdata	*mlxdata;
 	t_imgdata	*imdt;
-	t_point		*point_on_plane;
-	t_direction *plane_norm;
-	t_plane		*plane;
+	t_direction	*ray_direc;
+	t_ray		*ray;
 	int			pixel;
 	int			i;
 	int			j;
 
-	point_on_plane = point_coords(0, -20,0);
-	plane_norm = direction_components(0, 1, 0);
-	plane = plane_col_point_normal_dir(BLACK, point_on_plane, plane_norm);
 	mlxdata = (t_mlxdata *)malloc(sizeof(t_mlxdata));
 	imdt = (t_imgdata *)malloc(sizeof(t_imgdata));
 	mlxdata->mlx = mlx_init();
@@ -67,50 +54,60 @@ int	main(void)
 	imdt->image = mlx_new_image(mlxdata->mlx, WIN_WIDTH, WIN_HEIGHT);
 	imdt->address = mlx_get_data_addr(imdt->image, &imdt->bits_pp, \
 									   &imdt->line_len, &imdt->endian);
-	ambient = ambient_input(RED, 0.4);
-	cam_loc = point_coords(0, 0, 0);
-	cam_point = point_coords(0, 0, 1);
-	cam_view_dir = direction_two_points(cam_loc, cam_point);
-	cam = camera_input(cam_loc, cam_view_dir, 90);
-	screen = screen_camera(WIN_WIDTH, WIN_HEIGHT, cam);
-	sphere_centre = point_coords(10, 10, 0);
-	sphere = sphere_col_centre_radius(0x00FF5555, sphere_centre, 5);
+	// Scene and lighting
+	// t_ambient	*ambient = ambient_input(RED, 0.4);
+	t_point		*cam_loc = point_coords(0, 0, 0);
+	t_direction	*cam_dir = direction_components(0, 5, 0);
+	t_camera	*cam = camera_input(cam_loc, cam_dir, 90);
+	t_obj		*obj_cam = object_camera(cam);
+	object_print(obj_cam);
+	free(obj_cam);
+	t_screen	*screen = screen_camera(WIN_WIDTH, WIN_HEIGHT, cam);
+	t_obj		*obj_scr = object_screen(screen);
+	object_print(obj_scr);
+	free(obj_scr);
 	free_point(cam_loc);
-	free_point(cam_point);
-	free_direction(cam_view_dir);
+	free_direction(cam_dir);
+	// Plane
+	// t_point		*point_on_plane = point_coords(0, 100, 0);
+	// t_direction	*plane_norm = direction_components(0, 1, 0);
+	// t_plane		*plane = plane_col_point_normal_dir(0x00FF5555, point_on_plane, plane_norm);
+	// free_point(point_on_plane);
+	// free_direction(plane_norm);
+	// Sphere
+	t_point		*sphere_centre = point_coords(0, 1000, 0);
+	t_sphere	*sphere = sphere_col_centre_radius(0x00FF5555, sphere_centre, 100);
 	free_point(sphere_centre);
+	// Pre-Screen Tests
 	i = 0;
 	while(i < WIN_HEIGHT)
 	{
 		j = 0;
 		while(j < WIN_WIDTH)
 		{
-			ray = ray_two_points(cam->location, screen->pixels[i][j]->point);
-			// pixel = ray_sphere_intersection(ray, sphere);
-			pixel = ray_plane_intersection(ray, plane);
+			ray_direc = direction_two_points(cam->location, screen->pixels[i][j]->point);
+			ray = ray_start_dir(screen->pixels[i][j]->point, ray_direc);
+			free_direction(ray_direc);
+			pixel = ray_sphere_intersection(ray, sphere);
+			// pixel = intersection_ray_plane(ray, plane);
 			if (pixel == 0)
-				quick_put_pixel(imdt, j, i, ambient->ratio * ambient->colour);
+				quick_put_pixel(imdt, j, i, BLACK);
 			else
-				quick_put_pixel(imdt, j, i, colour_ambient((sphere->colour), ambient));
-			free(ray->ray_dir);
-			free(ray->ray_orig);
-			free(ray);
+				quick_put_pixel(imdt, j, i, WHITE);
+			free_ray(ray);
 			j++;
 		}
 		i++;
 	}
-	//Sphere colour in projection
-	printf("0x%08X\n", (unsigned int) colour_ambient((sphere->colour), ambient));
 	mlx_put_image_to_window(mlxdata->mlx, mlxdata->window, imdt->image, 0, 0);
 	mlx_hook(mlxdata->window, 17, NO_EVENT, closing, mlxdata);
 	mlx_hook(mlxdata->window, 3, KEY_RELEASE, keys, mlxdata);
 	mlx_loop(mlxdata->mlx);
-	free(plane->point);
-	free(plane->normal);
-	free(plane);
+	// free_ambient(ambient);
 	free_camera(cam);
 	free_screen(screen);
-	free_ambient(ambient);
+	// free_plane(plane);
+	free_sphere(sphere);
 	free(mlxdata);
 	free(imdt);
 	return (0);
