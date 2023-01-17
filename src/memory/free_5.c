b9ad9f6a35a62c4354e6d24bc976b90261a08af3
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-17 23:45:00 +0100

    added freeing for pixels and intersections, pixel freeing acts bad with intersection freeing

--- src/memory/free_5.c
+++ src/memory/free_5.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 16:14:07 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/01/17 14:38:27 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/01/17 23:32:45 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,20 @@ void	free_cylinder(t_cylinder *cylinder)
 void	free_ambient(t_ambient *ambient)
 {
 	free(ambient);
+}
+
+//Function frees an intersection.
+void	free_intersection(t_intersect *intersection)
+{
+	free_point(intersection->point);
+	free(intersection);
+}
+
+//Function frees a pixel.
+void	free_pixel(t_pixel *pixel)
+{
+	// if (pixel->intrsct->state == 1)
+	// 	free_intersection(pixel->intrsct);
+	free_point(pixel->point);
+	free(pixel);
 }
