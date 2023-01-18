89e84a7d67a22c7385204a2abd053070b61a54ed
Pablo Andalaft <pablo.andalaft@gmail.com>
2023-01-18 20:14:46 +0100

    added validator for atof input

--- src/input/valid.c
+++ src/input/valid.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 15:20:44 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/01/18 16:50:44 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/01/18 20:14:30 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,50 @@ int	is_valid_char(char ch)
 	if (is_space(ch) == 1)
 		return (1);
 	return (0);
+}
+
+//Function counts the number of period characters in a string.
+static int	num_period(const char *str)
+{
+	int	ct;
+	int	i;
+
+	ct = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '.')
+			ct++;
+		i++;
+	}
+	return (ct);
+}
+
+//Function counts the number of numeric sign characters in a string.
+static int	num_sign(const char *str)
+{
+	int	ct;
+	int	i;
+
+	ct = 0;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '-' || str[i] == '+')
+			ct++;
+		i++;
+	}
+	return (ct);
+}
+
+//Function determines whether a string is valid input for the atof function.
+int	valid_atof(const char *str)
+{
+	if (num_period(str) > 1)
+		return (0);
+	if (num_sign(str) > 1)
+		return (0);
+	if (num_sign(str) == 1 && !(str[0] == '+' || str[0] == '-'))
+		return (0);
+	return (1);
 }
