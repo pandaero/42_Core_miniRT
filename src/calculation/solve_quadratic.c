69606d66c470884384f4715391764e3a9e2b842d
Piotr Biedermann <piotr.biedermann84@gmail.com>
2023-02-06 21:39:16 +0100

    base and cylinder mantle work

--- src/calculation/solve_quadratic.c
+++ src/calculation/solve_quadratic.c
@@ -6,17 +6,28 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 16:32:02 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/01/28 16:57:20 by pbiederm         ###   ########.fr       */
+/*   Updated: 2023/02/06 20:26:39 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minirt.h"
 #include <math.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define REAL_SOLUTIONS_EXISTS 1
-#define ONE_OR_INFINITE_SOLUTION_EXISTS 2
-#define REAL_SOLUTIONS_NONE 0
+#define TWO_SOLUTIONS 1
+#define ONE_SOLUTION 2
+#define NO_REAL_SOLUTION 0
+
+static int invalid_input(double a)
+{
+	if (a == 0)
+	{
+		write(2, "Invalid quadratic equation input\n", 34);
+		return (1);
+	}
+	return(0);
+}
 
 /*ax^2 + bx + c* take ownership of x, x[0] states that there is a solution*/
 double	*solve_quadratic_real(double a, double b, double c)
@@ -24,24 +35,30 @@ double	*solve_quadratic_real(double a, double b, double c)
 	double	discriminant;
 	double	*x;
 
+	if (invalid_input(a))
+		return(NULL);
 	x = (double*)malloc(3 * sizeof(double));
 	if (!x)
 		return(NULL);
 	discriminant = pow(b, 2) - 4 * (a) * (c);
 	if (discriminant > 0)
 	{
-		x[0] = REAL_SOLUTIONS_EXISTS;
+		x[0] = TWO_SOLUTIONS;
 		x[1] = (-b + sqrt(discriminant)) / (a * 2);
 		x[2] = (-b - sqrt(discriminant)) / (a * 2);
 	}
 	else if (discriminant == 0)
 	{
-		x[0] = ONE_OR_INFINITE_SOLUTION_EXISTS;
+		x[0] = ONE_SOLUTION;
 		x[1] = (-b) / (a * 2);
+		x[2] = x[1];
 	}
 	else if (discriminant < 0)
 	{
-		x[0] = REAL_SOLUTIONS_NONE;
+		x[0] = NO_REAL_SOLUTION;
+		x[1] = 0;
+		x[2] = 0;
+		x[3] = 0;
 	}
 	return (x);
 }
--- src/intersection/intersect_cylinder.c
+++ src/intersection/intersect_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 17:25:29 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/02/06 19:01:21 by pbiederm         ###   ########.fr       */
+/*   Updated: 2023/02/06 21:38:37 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double *solve_quadratic(double a, double b, double c)
-{
-	double *result;
-	double discriminant;
-
-	result = (double*)malloc(3 * sizeof(double));
-	discriminant = pow(b, 2) - (4 * a * c);
-	if (discriminant > 0)
-	{
-		result[0] = 1;
-		
-	}
-}
-
 t_intersect	*intersection_ray_cylinder(t_ray *ray, t_cylinder *cylinder)
 {
+	t_intersect	*intersection_data;
+	t_vector	*vector_ray;
+	t_vector	*vector_cylinder;
+	t_vector	*origin_base_center;
+	double		coefficient_a;
+	double		coefficient_b;
+	double		coefficient_c;
+	double		*quadratic_solutions;
+	double		distance_from_base;
 
+	distance_from_base = 0;
+	intersection_data = (t_intersect*)malloc(sizeof(t_intersect));
+	intersection_data->point = (t_point*)malloc(sizeof(t_point));
+	intersection_data->point->x_co = 0;
+	intersection_data->point->y_co = 0;
+	intersection_data->point->z_co = 0;
+	vector_ray = vector_scale_direction(1, ray->ray_dir);
+	vector_cylinder = vector_scale_direction(1, cylinder->orientation);
+	origin_base_center = vector_two_points(ray->ray_orig, cylinder->centre);
+	coefficient_a = vector_dot(vector_ray, vector_ray) -\
+	pow(vector_dot(vector_ray, vector_cylinder), 2);
+	coefficient_b = 2 * (vector_dot(vector_ray, origin_base_center) - (vector_dot(vector_ray, vector_cylinder) * vector_dot(origin_base_center, vector_cylinder)));
+	coefficient_c = vector_dot(origin_base_center, origin_base_center) - pow(vector_dot(origin_base_center, vector_cylinder),2) - pow(cylinder->radius, 2);
+	quadratic_solutions = solve_quadratic_real(coefficient_a, coefficient_b, coefficient_c);
+	if(quadratic_solutions[0] == 0)
+	{
+		intersection_data->state = 0;
+		return(intersection_data);
+	}
+	else if(quadratic_solutions[0] == 1 && quadratic_solutions[1] > quadratic_solutions[2])
+	{
+		intersection_data->point = get_intersection_point(ray, quadratic_solutions[1]);
+		distance_from_base = vector_dot(vector_ray, vector_cylinder) * quadratic_solutions[1] + vector_dot(origin_base_center, vector_cylinder);
+	}
+	else if(quadratic_solutions[0] == 1 && quadratic_solutions[2] > quadratic_solutions[1])
+	{
+		intersection_data->point = get_intersection_point(ray, quadratic_solutions[2]);
+		distance_from_base = vector_dot(vector_ray, vector_cylinder) * quadratic_solutions[2] + vector_dot(origin_base_center, vector_cylinder);
+	}
+	else if(quadratic_solutions[0] == 2)
+	{
+		intersection_data->point = get_intersection_point(ray, quadratic_solutions[1]);
+		distance_from_base = vector_dot(vector_ray, vector_cylinder) * quadratic_solutions[1] + vector_dot(origin_base_center, vector_cylinder);
+	}
+	if (distance_from_base < 0)
+	{
+	 	// base cap
+		t_plane		*lower_cap = plane_col_point_normal_dir(0x00FF00, cylinder->centre, cylinder->orientation); // direction could be inverted
+		t_intersect	*cap_intersection = intersection_ray_plane(ray, lower_cap);
+		if (cap_intersection->state != 0)
+		{
+			t_vector	*disc_center_to_point_plane = vector_two_points(cylinder->centre, cap_intersection->point);
+			double		distance_center_point2 = vector_dot(disc_center_to_point_plane, disc_center_to_point_plane);
+			double		radius2 = cylinder->radius * cylinder->radius;
+			if (distance_center_point2 > radius2)
+			{
+				free(intersection_data->point);
+				free(intersection_data);
+				cap_intersection->state = 0;
+				return(cap_intersection);
+			}
+		}
+		if (cap_intersection->state == 0)
+		{
+			free(cap_intersection->point);
+			free(cap_intersection);
+			intersection_data->state = 0;
+			return(intersection_data);
+		}
+		// base cap
+	}
+	if (distance_from_base > cylinder->height)
+	{
+		intersection_data->state = 0;
+		return (intersection_data);
+	}
+	intersection_data->state = 1;
+	free(quadratic_solutions);
+	free(vector_ray);
+	free(vector_cylinder);
+	free(origin_base_center);
+	return(intersection_data);
 }
