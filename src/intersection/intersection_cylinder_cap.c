ac2fe4388c943177dd87566804e6b0f9d15a9210
Piotr Biedermann <piotr.biedermann84@gmail.com>
2023-02-07 19:17:04 +0100

    fixed one file with 25 lines

--- src/intersection/intersection_cylinder_cap.c
+++ src/intersection/intersection_cylinder_cap.c
@@ -6,13 +6,14 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 16:32:58 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/02/07 17:52:01 by pbiederm         ###   ########.fr       */
+/*   Updated: 2023/02/07 19:16:34 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minirt.h"
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void	free_cylinder_plane(t_cylinder_cap *t)
 {
@@ -27,17 +28,35 @@ void	free_cylinder_cap(t_cylinder_cap *t)
 	free(t);
 }
 
+void	is_ray_within_radius(t_cylinder_cap *t, t_point *center, \
+t_cylinder *cylinder, t_intersect *cap_intersection)
+{
+	t->disc_center_to_point_plane = vector_two_points(center, \
+	cap_intersection->point);
+	t->distance_center_point_sq = vector_dot(t->disc_center_to_point_plane, \
+	t->disc_center_to_point_plane);
+	t->radius_sq = cylinder->radius * cylinder->radius;
+}
+
+static t_cylinder_cap	*cap_init(t_point *center, t_cylinder *cylinder)
+{
+	t_cylinder_cap	*t;
+
+	t = (t_cylinder_cap *)malloc(sizeof(t_cylinder_cap));
+	t->cap_plane = plane_col_point_normal_dir(0x00FF00, \
+	center, cylinder->orientation);
+	t->disc_center_to_point_plane = NULL;
+	return (t);
+}
+
 t_intersect	*intersection_cylinder_cap(t_ray *ray, \
 t_point *center, t_cylinder *cylinder)
 {
 	t_cylinder_cap	*t;
 	t_intersect		*cap_intersection;
 
-	t = (t_cylinder_cap *)malloc(sizeof(t_cylinder_cap));
-	t->cap_plane = plane_col_point_normal_dir(0x00FF00, \
-	center, cylinder->orientation);
+	t = cap_init(center, cylinder);
 	cap_intersection = intersection_ray_plane(ray, t->cap_plane);
-	t->disc_center_to_point_plane = NULL;
 	if (cap_intersection->state == 0)
 	{
 		free_cylinder_plane(t);
@@ -45,11 +64,7 @@ t_point *center, t_cylinder *cylinder)
 	}
 	if (cap_intersection->state != 0)
 	{
-		t->disc_center_to_point_plane = vector_two_points(center, \
-		cap_intersection->point);
-		t->distance_center_point_sq = vector_dot(t->disc_center_to_point_plane, \
-		t->disc_center_to_point_plane);
-		t->radius_sq = cylinder->radius * cylinder->radius;
+		is_ray_within_radius(t, center, cylinder, cap_intersection);
 		if (t->distance_center_point_sq > t->radius_sq)
 		{
 			cap_intersection->state = 0;
