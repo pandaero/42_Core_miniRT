4217c3e14e15558b7f8cb76dbbc172936d83dcef
piotr biedermann <piotr.biedermann84@gmail.com>
2023-02-19 14:43:53 +0100

    working on distance from base to point intersection

--- src/intersection/cylinder_redo.c
+++ src/intersection/cylinder_redo.c
@@ -6,7 +6,7 @@
 /*   By: pbiederm <pbiederm@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 18:38:13 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/02/19 09:24:24 by pbiederm         ###   ########.fr       */
+/*   Updated: 2023/02/19 14:39:44 by pbiederm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,25 @@
 
 
 
+// //Substracts 
+// static t_vect	sub_vect(const t_vector *vect1, const t_vector *vect2)
+// {
+// 	vec
+// 	return (new_vect(vect1.x - vect2.x, vect1.y - vect2.y, vect1.z - vect2.z));
+// }
+
+// generates the distance between intersection and base
+static double	distance_base_intersection(t_vector *vector_ray, t_vector *vector_cylinder, t_vector *vector_base_center, double quadratic_solution\
+)
+{
+	double distance_from_base;
+	double v = vector_dot(vector_ray, \
+	vector_cylinder);
+	double w = vector_dot(vector_base_center, vector_cylinder);
+	distance_from_base = v * quadratic_solution + w;
+	return(distance_from_base);
+}
+
 t_intersect	*intersection_ray_cylinder(t_ray *ray, t_cylinder *cylinder)
 {
 	t_intersect	*cylinder_intersect;
@@ -33,7 +52,6 @@ t_intersect	*intersection_ray_cylinder(t_ray *ray, t_cylinder *cylinder)
 	t_vector	*vector_cylinder;
 	t_vector	*vector_ray_origin_base_center;
 	t_vector	*vector_base_top;
-	t_vector	*vector_base_intersection;
 	double		distance_cylinder_axis;
 	double		coefficient[3];
 	double		*quadratic_result;
@@ -52,41 +70,48 @@ t_intersect	*intersection_ray_cylinder(t_ray *ray, t_cylinder *cylinder)
 	if ((quadratic_result[0] == 2) && (quadratic_result[1] > 0 )&& (quadratic_result[2] > 0))
 	{
 		cylinder_intersect->point = get_intersection_point(ray, quadratic_result[1]);
- 	}
+		distance_cylinder_axis = vector_dot(vector_cylinder, vector_substract(vector_scale_direction(quadratic_result[1], vector_ray), vector_substract(cylinder->centre, ray->ray_orig)));
+	}
 	if ((quadratic_result[0] == 1) && (quadratic_result[1] > 0 )&& (quadratic_result[2] < 0))
 	{
 		cylinder_intersect->point = get_intersection_point(ray, quadratic_result[1]);
+		distance_cylinder_axis = vector_dot(vector_cylinder, vector_substract(vector_scale_direction(quadratic_result[1], vector_ray), vector_substract(cylinder->centre, ray->ray_orig)));
  	}
 	else if ((quadratic_result[0] == 1) && (quadratic_result[1] > 0 )&& (quadratic_result[2] < 0))
 	{
 		cylinder_intersect->point = get_intersection_point(ray, quadratic_result[1]);
+		distance_cylinder_axis = vector_dot(vector_cylinder, vector_substract(vector_scale_direction(quadratic_result[1], vector_ray), vector_substract(cylinder->centre, ray->ray_orig)));
 	}
 	else if ((quadratic_result[0] == 1) && (quadratic_result[2] < quadratic_result[1] )&& (quadratic_result[2] > 0))
 	{
 		cylinder_intersect->point = get_intersection_point(ray, quadratic_result[2]);
+		distance_cylinder_axis = vector_dot(vector_cylinder, vector_substract(vector_scale_direction(quadratic_result[2], vector_ray), vector_substract(cylinder->centre, ray->ray_orig)));
 	}
 	else if ((quadratic_result[0] == 1) && (quadratic_result[1] < quadratic_result[2]) && (quadratic_result[1] > 0))
 	{
-		cylinder_intersect->point = get_intersection_point(ray, quadratic_result[2]);
+		cylinder_intersect->point = get_intersection_point(ray, quadratic_result[1]);
+		distance_cylinder_axis = vector_dot(vector_cylinder, vector_substract(vector_scale_direction(quadratic_result[1], ray->ray_dir), vector_two_points(cylinder->centre, ray->ray_orig)));
 	}
 	if(cylinder_intersect->point != NULL)
-	{
-		vector_base_intersection = vector_two_points(cylinder->centre, cylinder_intersect->point);
-		distance_cylinder_axis = vector_dot(vector_base_intersection, vector_base_top);
-		// printf("distance: [%f] |", distance_cylinder_axis);
-		// printf("cylinder->heigt: %f |");
-		// printf("cylinder points: [%f, %f, %f]", cylinder_intersect->point->x_co, cylinder_intersect->point->y_co, cylinder_intersect->point->z_co);
-		// printf("[%f]", cylinder_intersect->point->x_co);
-		if (distance_cylinder_axis >= 0 && distance_cylinder_axis <= cylinder->height)
+	{		
+		printf("distance: [%f] |", distance_cylinder_axis);
+		printf("cylinder->heigt: %f |", cylinder->height);
+		printf("cylinder points: [%f, %f, %f]", cylinder_intersect->point->x_co, cylinder_intersect->point->y_co, cylinder_intersect->point->z_co);
+		printf("[%f]", cylinder_intersect->point->x_co);
+		if (distance_cylinder_axis >= 0.0)
 		{
-			printf("bang!\n");
-			cylinder_intersect->state = 1;
+			if (distance_cylinder_axis <= cylinder->height)
+			{
+				printf("bang!\n");
+				cylinder_intersect->state = 1;
+				
+			}
 		}
 		printf("\n");
 	}
 	if(cylinder_intersect->state == 1)
 	{
-		free_vector(vector_base_intersection);
+		//may be a memory problem somewhere here
 		free_vector(vector_base_top);
 		free_vector(vector_ray_origin_base_center);
 		free_vector(vector_cylinder);
@@ -95,6 +120,7 @@ t_intersect	*intersection_ray_cylinder(t_ray *ray, t_cylinder *cylinder)
 		return(cylinder_intersect);
 	}
 	cylinder_intersect->state = 0;
+	free(cylinder_intersect->point);
 	cylinder_intersect->point = NULL;
 	free_vector(vector_base_top);
 	free_vector(vector_ray_origin_base_center);
@@ -103,3 +129,44 @@ t_intersect	*intersection_ray_cylinder(t_ray *ray, t_cylinder *cylinder)
 	free(quadratic_result);
 	return(cylinder_intersect);
 }
+
+// double dist1 = vector_dot(vector_cylinder, vector_substract(vector_scale_direction(quadratic_result[1], vector_ray), vector_substract(cylinder->centre, ray->ray_orig)));
+
+// 	t_vect	add_vect(const t_vect vect1, const t_vect vect2)
+// {
+// 	return (new_vect(vect1.x + vect2.x, vect1.y + vect2.y, vect1.z + vect2.z));
+// }
+
+// static t_vect	sub_vect(const t_vecor vect1, const t_vector vect2)
+// 
+// 	return (new_vect(vect1.x - vect2.x, vect1.y - vect2.y, vect1.z - vect2.z));
+// }
+
+// t_vect	minus_vect(const t_vect vect)
+// {
+// 	return (new_vect(-vect.x, -vect.y, -vect.z));
+// }
+
+// t_vect	multi_vect(const t_vect vect, const double x)
+// {
+// 	return (new_vect(vect.x * x, vect.y * x, vect.z * x));
+// }
+
+// double	dot_product(const t_vect vect1, const t_vect vect2)
+// {
+// 	return (vect1.x * vect2.x + vect1.y * vect2.y + vect1.z *vect2.z);
+// }
+
+// t_vect	cross_product(const t_vect v1, const t_vect v2)
+// {
+// 	t_vect	origin;
+// 	t_vect	normal;
+// 	double	denom;
+// 	double	cross;
+
+// 	origin = new_vect(0, 0, 0);
+// 	denom = distance(origin, v1) * distance(origin, v2);
+// 	cross = denom * sinf(acos(dot_product(v1, v2) / denom));
+// 	normal = new_vect(v1.y * v2.z - v1.z * v2.y, v1.x * v2.z - v1.z * v2.x, v1.y * v2.x - v1.x * v2.y);
+// 	return (multi_vect(normal, cross));
+// }
