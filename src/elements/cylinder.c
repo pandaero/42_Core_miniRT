a2a6ffe8f0b1eeadb8079ff310ffff702ca3698b
pandalaf <pandalaf@student.42wolfsburg.de>
2023-03-10 13:58:55 +0100

    added cylinder caps

--- src/elements/cylinder.c
+++ src/elements/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 16:31:42 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/02/02 19:11:25 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/03/10 13:56:57 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,34 @@ t_cylinder	*cylinder_create(void)
 	new->height = 0;
 	new->centre = NULL;
 	new->orientation = NULL;
+	new->top_cap = NULL;
+	new->base_cap = NULL;
 	return (new);
+}
+
+//Function assigns disc caps to a cylinder.
+static void	give_caps(t_cylinder *cyl)
+{
+	t_point	*top_centre;
+	t_point	*base_centre;
+	t_vector	*vec_centre_top;
+	t_vector	*vec_centre_base;
+	t_direction	*neg_axis;
+
+	vec_centre_top = vector_scale_direction(cyl->height / 2, cyl->orientation);
+	neg_axis = reverse_direction(cyl->orientation);
+	vec_centre_base = vector_scale_direction(cyl->height / 2, neg_axis);
+	top_centre = point_point_vector(cyl->centre, vec_centre_top);
+	base_centre = point_point_vector(cyl->centre, vec_centre_base);
+	cyl->top_cap = disc_centre_normal_radius_colour(top_centre, cyl->orientation, \
+				cyl->radius, cyl->colour);
+	cyl->base_cap = disc_centre_normal_radius_colour(base_centre, cyl->orientation, \
+				cyl->radius, cyl->colour);
+	free_vector(vec_centre_top);
+	free_vector(vec_centre_base);
+	free_direction(neg_axis);
+	free_point(top_centre);
+	free_point(base_centre);
 }
 
 //Function creates a defined cylinder from centre, radius and height.
@@ -38,6 +65,7 @@ t_cylinder	*cylinder_centre_orient_radius_height(t_point *centre, \
 	cylinder->height = height;
 	cylinder->centre = point_copy(centre);
 	cylinder->orientation = direction_copy(orientation);
+	give_caps(cylinder);
 	return (cylinder);
 }
 
@@ -72,6 +100,7 @@ t_cylinder	*cylinder_line(const char *line)
 	new->radius = ft_atof(split[3]) / 2;
 	new->height = ft_atof(split[4]);
 	new->colour = colour_str(split[5]);
+	give_caps(new);
 	free_split(split);
 	return (new);
 }
--- src/elements/disc.c
+++ src/elements/disc.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 21:48:28 by pbiederm          #+#    #+#             */
-/*   Updated: 2023/02/17 01:09:55 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/03/10 13:44:14 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,20 @@ t_disc	*disc_centre_normal_radius_colour(t_point *centre, t_direction *norm, \
 		return (NULL);
 	new->centre = point_copy(centre);
 	new->normal = direction_copy(norm);
+	new->radius = radius;
+	new->colour = colour_copy(colour);
+	return (new);
+}
+
+//Function creates a disc element from a centre, plane, radius, and colour.
+t_disc	*disc_centre_plane_radius_colour(t_point *centre, t_plane *plane, \
+											double radius, t_colour *colour)
+{
+	t_disc	*new;
+
+	new = disc_create();
+	new->centre = point_copy(centre);
+	new->normal = direction_copy(plane->normal);
 	new->radius = radius;
 	new->colour = colour_copy(colour);
 	return (new);
