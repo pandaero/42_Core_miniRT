e35db3d8ae8b4316d819dfba03b9b631b45eef11
piotr biedermann <piotr.biedermann84@gmail.com>
2023-03-12 16:15:51 +0100

    removed memory leak by dividing the data structure into the one returned by the function and the one freed inside the function, in the previous solution the struct was freed before beeing returned

--- src/intersection/intersect_sphere.c
+++ src/intersection/intersect_sphere.c
@@ -17,41 +17,41 @@
 
 //Function assigns the intersection point and surface normal for the point.
 static void	sphere_intersection_point_normal(t_ray *ray, t_sphere *sphere, \
-											t_itsct_sphere *is)
+											t_intersect *itsct)
 {
 	t_direction	*temp;
 
-	is->itsct->point = point_ray_distance(ray, is->itsct->distance);
-	is->itsct->normal = direction_two_points(sphere->centre, is->itsct->point);
-	if (direction_dot(is->itsct->normal, ray->ray_dir) > 0)
+	itsct->point = point_ray_distance(ray, itsct->distance);
+	itsct->normal = direction_two_points(sphere->centre, itsct->point);
+	if (direction_dot(itsct->normal, ray->ray_dir) > 0)
 	{
-		temp = is->itsct->normal;
-		is->itsct->normal = direction_reverse(temp);
+		temp = itsct->normal;
+		itsct->normal = direction_reverse(temp);
 		free(temp);
 	}
 }
 
 //Function handles the case where there are two solutions for ray-sphere itsct.
 static void	sphere_intersection_two(t_ray *ray, t_sphere *sphere, \
-									t_itsct_sphere *is)
+									t_itsct_sphere *is, t_intersect *itsct)
 {
 	if (is->soln->first < 0 && is->soln->second < 0)
 	{
-		is->itsct->state = MISSED;
+		itsct->state = MISSED;
 		return ;
 	}
 	else if ((is->soln->first > 0 || is->soln->second > 0) && \
 			(is->soln->first < 0 || is->soln->second < 0))
 	{
-		is->itsct->state = INTERSECTED;
-		is->itsct->distance = fmax(is->soln->first, is->soln->second);
+		itsct->state = INTERSECTED;
+		itsct->distance = fmax(is->soln->first, is->soln->second);
 	}
 	else
 	{
-		is->itsct->state = INTERSECTED;
-		is->itsct->distance = fmin(is->soln->first, is->soln->second);
+		itsct->state = INTERSECTED;
+        itsct->distance = fmin(is->soln->first, is->soln->second);
 	}
-	sphere_intersection_point_normal(ray, sphere, is);
+	sphere_intersection_point_normal(ray, sphere, itsct);
 }
 
 //Function performs a sign change and assigns solutions to intersect solution.
@@ -70,6 +70,7 @@ t_intersect	*intersection_ray_sphere(t_ray *ray, t_sphere *sphere)
 	t_itsct_sphere	*is;
 	t_quad_cof		*quad;
 	t_quad_sol		*soln;
+    t_intersect     *itsct;
 
 	is = (t_itsct_sphere *)malloc(sizeof(t_itsct_sphere));
 	is->ray_to_ctr = vector_two_points(ray->ray_orig, sphere->centre);
@@ -81,19 +82,19 @@ t_intersect	*intersection_ray_sphere(t_ray *ray, t_sphere *sphere)
 						pow(sphere->radius, 2);
 	soln = solve_quadratic(quad);
 	modify_sphere_quad(quad, soln, is);
-	is->itsct = intersect_create();
+	itsct = intersect_create();
 	if (soln->sol == NO_REAL)
-		is->itsct->state = MISSED;
+		itsct->state = MISSED;
 	else if (soln->sol == ONE)
 	{
-		is->itsct->state = INTERSECTED;
-		is->itsct->distance = -quad->linear / (2 * quad->squared);
-		sphere_intersection_point_normal(ray, sphere, is);
+		itsct->state = INTERSECTED;
+		itsct->distance = -quad->linear / (2 * quad->squared);
+		sphere_intersection_point_normal(ray, sphere, itsct);
 	}
 	else
-		sphere_intersection_two(ray, sphere, is);
+		sphere_intersection_two(ray, sphere, is, itsct);
 	free(quad);
 	free(soln);
 	free_is(is);
-	return (is->itsct);
+	return (itsct);
 }
