99820b3eb3e8d4e71eb0b5511fd74c24367d0262
Pablo Andalaft Tarodo <pandalaf@student.42wolfsburg.de>
2023-03-14 13:06:32 +0100

    working shadows

--- src/program/render_3.c
+++ src/program/render_3.c
@@ -6,7 +6,7 @@
 /*   By: pandalaf <pandalaf@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 13:24:52 by pandalaf          #+#    #+#             */
-/*   Updated: 2023/03/14 12:55:37 by pandalaf         ###   ########.fr       */
+/*   Updated: 2023/03/14 13:05:10 by pandalaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,17 +41,6 @@ static int	sec_int_loop(t_sec_itsct_pass *stct, t_pixel *pixel, t_program *progr
 	return (0);
 }
 
-static int	is_render(t_obj *object)
-{
-	if (object->elem == PLANE)
-		return (1);
-	if (object->elem == SPHERE)
-		return (1);
-	if (object->elem == CYLINDER)
-		return (1);
-	return (0);
-}
-
 //Function calculates a secondary intersection for a pixel.
 void	secondary_intersection_pass(t_program *program, t_pixel *pixel)
 {
@@ -66,8 +55,6 @@ void	secondary_intersection_pass(t_program *program, t_pixel *pixel)
 	stct.ray = ray_start_dir(pixel->itsct.point, stct.dir);
 	while (stct.obj && stct.sec_unren > 0)
 	{
-		while (is_render(stct.obj))
-			stct.obj = stct.obj->next;
 		if (stct.obj && stct.obj == pixel->itsct.object)
 		{
 			stct.obj = stct.obj->next;
